9bd1654890aee0d77d757023fb1a36f6a749fbc6
Austin Clift <austinclift@outlook.com>
2015-02-10 13:03:16 -0700

    [axof: 224][axof: 225] Setup SASS.  Changed GRUNT to import Bootstrap SASS.  Imported Font-Awesome SASS. Setup Site Vars & Colors.  Setup mobile navigation scripts with jQuery and jQuery UI.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,55 @@ module.exports = function(grunt) {
         }
       }
     },
+    
+    // Copy Files
+    copy: {
+        
+        // Copy Only Needed Bootstrap Files
+        bootstrap: {
+            expand: true,
+            nonull: true,
+            src: [
+                'app/lib/bootstrap-sass/assets/stylesheets/bootstrap/_grid.scss',
+                'app/lib/bootstrap-sass/assets/stylesheets/bootstrap/_mixins.scss',
+                'app/lib/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss'
+            ],
+            dest: 'app/css/partials/bootstrap',
+            flatten: true,
+            filter: 'isFile'
+        },
+        
+        // Copy Bootstrap Mixins
+        bootstrapMixins: {
+            expand: true,
+            nonull: true,
+            src: 'app/lib/bootstrap-sass/assets/stylesheets/bootstrap/mixins/*.scss',
+            dest: 'app/css/partials/bootstrap/mixins',
+            flatten: true,
+            filter: 'isFile'
+        },
+        
+        // Copy Font Awesome Fonts
+        fonts: {
+            expand: true,
+            nonull: true,
+            src: 'app/lib/font-awesome-sass/assets/fonts/**',
+            dest: 'app/fonts',
+            flatten: true,
+            filter: 'isFile'
+        },
+        
+        // Copy Font Awesome SASS
+        fontawesomeSASS: {
+            expand: true,
+            nonull: true,
+            src: 'app/lib/font-awesome-sass/assets/stylesheets/font-awesome/*.scss',
+            dest: 'app/css/partials/fontawesome',
+            flatten: true,
+            filter: 'isFile'
+        }
+        
+    },
 
     sass: {
        dist: {
@@ -47,6 +96,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
