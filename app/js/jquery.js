ba48dc568c527510f80e69af36d7725c972df39a
Austin Clift <austinclift@outlook.com>
2015-02-10 21:42:01 -0700

    Added tablet navigation styles and footer styles.  SASSified (nested) css

--- app/js/jquery.js
+++ app/js/jquery.js
@@ -4,10 +4,14 @@
 jQuery(document).ready(function(){
     
     // Call Functions
-    toggle_class('header .nav .drawer', 'header .nav nav');
-    toggle_class('header .nav nav ul li a', 'header .nav nav');
-    toggle_class('header .nav .drawer', '.app-wrapper');
-    toggle_class('header .nav nav ul li a', '.app-wrapper');
+    toggle_class('header .menu .drawer', 'header .menu nav');
+    toggle_class('header .menu nav ul li a', 'header .menu nav');
+    toggle_class('header .menu .drawer', '.app-wrapper');
+    toggle_class('header .menu nav ul li a', '.app-wrapper');
+    toggle_class('header .menu .drawer', 'footer');
+    toggle_class('header .menu nav ul li a', 'footer');
+    toggle_class('header .menu .user', 'header .menu .user .extra');
+    toggle_class('header .menu .user', 'header .menu .user');
     
     // Toggle Class
     function toggle_class(trigger, target){
