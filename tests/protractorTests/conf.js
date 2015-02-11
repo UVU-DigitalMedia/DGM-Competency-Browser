5748d9fc07763c82c4889e188c602423e66cd2fc
Jordan <jordanlarkin24@gmail.com>
2015-02-10 22:06:14 -0700

    Added config and spec files for protractor and dependencies[axof:218 wl: 2 hours]

---
+++ tests/protractorTests/conf.js
@@ -0,0 +1,11 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+ multiCapabilities: [{
+     browserName: 'firefox'
+   }, {
+     browserName: 'chrome'
+   }, {
+     browserName: 'safari'
+   }]
+ }
