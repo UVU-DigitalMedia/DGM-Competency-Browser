b6f800105b5a9b613ef26e065775b72d97e5c00a
Josh M. Diaz <joshua.michael.diaz@gmail.com>
2015-02-19 17:32:01 -0700

    I fixed the mongoose schema for the 3 schema files. They are no longer just json data. I modified the server.js file to include the schema files.

--- models/jobs.js
+++ models/jobs.js
@@ -1,23 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var jobsSchema = new Schema({
-  {
-    "id": "1001",
-    "name": "Assistant Technical Artist - Contributor (TA)",
-    "requiredSkills": ["2009, 2005, 2001, 2004"],
-    "categories": ["3013", "3014", "3004"]
-  },
-  {
-    "id": "1002",
-    "name": "Associate Technical Artist - Contributor (TA)",
-    "requiredSkills": ["2004, 2019, 2075, 2045"],
-    "categories": ["3001", "3005", "3004"]
-  },
-  {
-    "id": "1003",
-    "name": "Technical Artist - Contributing Leader (TA)",
-    "requiredSkills": ["2043, 2024, 2015, 2005"],
-    "categories": ["3013", "3014", "3004"]
-  }
+var JobsSchema = new Schema({
+
+    id: Number,
+    name: String,
+    requiredSkills: [SkillsSchema],
+    categories: [CategorySchema],
+    createdDate: Date,
+    updated: { type: Date, default: Date.now }
+
 })
+
+module.exports = mongoose.model('Jobs', JobsSchema);
